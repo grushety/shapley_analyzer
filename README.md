e8b6518996a47c56375e300fe59faef5dbd215f4
grushety <yulia.v.grushetskaya@gmail.com>
2022-07-19 09:41:40 +0200

    remove file

--- README.md
+++ README.md
@@ -5,6 +5,14 @@ The goal of this study project was to build and train a model that classifies fl
 and to estimate the contribution of each input feature (individual bands of the remote sensing images) to the classification result
 through calculating the Shapley Value for each input feature.
 
-We have tried to implement a simple library to calculate shapely values, noisy the data and checked how valuable each feature's impact is.
+We have tried to implement a simple library to calculate shapely values, noised the data and checked how valuable each feature's impact is.
 
+### Models
+**UNET**  
+Unet represents one of the popular approaches for image segmentation models: to follow an encoder/decoder structure where we downsample the spatial resolution of the input, developing lower-resolution feature mappings which are learned to be highly efficient at discriminating between classes, and the upsample the feature representations into a full-resolution segmentation map.
+![img/u-net-architecture.png](img/u-net-architecture.png)
+
+**FCN**  
+FCN is a network that does not contain any “Dense” layers (as in traditional CNNs) instead it contains 1x1 convolutions that perform the task of fully connected layers (Dense layers).
+![img/fcn_architecture.png](img/fcn_architecture.png)
 
--- segmentation_models/utils/utils_functions.py
+++ segmentation_models/utils/utils_functions.py
@@ -1,14 +1,40 @@
+from random import randrange
+
 import matplotlib.pyplot as plt
 import math
+import numpy as np
 
 
 def show_images(image_arr, label_arr):
     n_lines = math.ceil(len(image_arr)/4)
     print(n_lines)
-    fig, arr = plt.subplots(n_lines, 4, sharex=True, figsize=(15, 15))
+    fig, arr = plt.subplots(n_lines, 4, figsize=(15, 15))
+    arr[-1, -1].axis('off')
+    arr[-1, -2].axis('off')
     current_line = 0
     for i in range(len(image_arr)):
         arr[current_line, i-(current_line*4)].imshow(image_arr[i])
         arr[current_line, i-(current_line*4)].set_title(label_arr[i])
         if (1 + current_line)*4 <= i+1:
             current_line += 1
+
+
+def noise_image(data, channels):
+    images = np.copy(data)
+    channels_to_noise = [i for i in "rgb" if(i not in channels)]
+
+    # calculade range for channels based on real data
+    r_min, r_max = np.min(images[:,:,:,0]), np.max(images[:,:,:,0])
+    g_min, g_max = np.min(images[:,:,:,1]), np.max(images[:,:,:,1])
+    b_min, b_max = np.min(images[:,:,:,2]), np.max(images[:,:,:,2])
+
+    for image in images:
+        for line in image:
+            for pixel in line:
+                if 'r' in channels_to_noise:
+                    pixel[0] = randrange(r_min, r_max)
+                if 'g' in channels_to_noise:
+                    pixel[1] = randrange(g_min, g_max)
+                if 'b' in channels_to_noise:
+                    pixel[2] = randrange(b_min, b_max)
+    return images
